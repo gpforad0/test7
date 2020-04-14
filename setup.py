b496e9657b57bbe8c4ba76fbc2219e1025bd4ecb
gpforad0 <gpforad0@gmail.com>
2020-04-14 11:44:20 -0400

    my first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import maintest2
+
+def main():
+    maintest2.username = 'gpforad0'
+    maintest2.email = 'gpforad0@gmail.com'
+    maintest2.repopath = 'git remote add origin https://github.com/gpforad0/test7.git'
+    maintest2.set()
+
+
+if __name__== '__main__':
+    main()
